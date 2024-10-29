c78b25af79e9bdd4c79ba37d02187fb2599d9266
Youssef Gaafar <yousfjaafer@gmail.com>
2024-10-30 01:01:42 +0300

    ➕ ADD: Implement Game UI and Logic with Validations

--- script.js
+++ script.js
@@ -1,128 +1,140 @@
-// Getting Computer Input
+const roundsButton = document.getElementById("rounds-button");
+const roundsInput = document.getElementById("rounds-input");
+const inputButtons = document.querySelectorAll(".player-buttons button");
+const scoresBox = document.querySelector(".scores");
+const playerScoreSpan = document.querySelector(".player-score span");
+const computerScoreSpan = document.querySelector(".computer-score span");
+const computerInputField = document.querySelector(".computer-input h2");
+const choices = ["rock", "paper", "scissors"];
+const winnerBox = document.querySelector(".winner");
+const winnerPhrase = document.querySelector(".winner h2");
+const resetButton = document.querySelector(".winner button");
+
+let playerScore = parseInt(playerScoreSpan.textContent);
+let computerScore = parseInt(computerScoreSpan.textContent);
+let rounds = 0;
+
 function getComputerChoice() {
-    let choices = ["rock", "paper", "scissors"];
     return choices[Math.floor(Math.random() * choices.length)];
 }
-// console.log("Compuer Choice Is:", getComputerChoice());
 
-//Getting Human Input
-function getHumanChoice() {
-    let humanChoice = prompt("Choose from the following: (Rock, Paper, Scissors) ");
-    humanChoice = humanChoice.toLowerCase();
-    while (humanChoice != "rock" && humanChoice != "paper" && humanChoice != "scissors") {
-        humanChoice = prompt("NO 😡.... Choose Again: (Rock, Paper, Scissors)");
-        humanChoice = humanChoice.toLowerCase();
-    }
-    return humanChoice;
+function updateScoreBoard() {
+    playerScoreSpan.textContent = playerScore;
+    computerScoreSpan.textContent = computerScore;
 }
-// console.log("Your Choice Is:", getHumanChoice());
 
-// Main Algorithm
-function playRound(hs, cs) {
-    let computerChoice = getComputerChoice();
-    let humanChoice = getHumanChoice();
-    console.log(`Computer Random Selection: ${computerChoice}`);
-    console.log(`Your Selection: ${humanChoice}`);
-    if (computerChoice === humanChoice) {
-        console.log("TIED");
+function displayResult() {
+    console.log("The END...");
+    winnerBox.style.cssText = "display: block;";
+    inputButtons.forEach(button => button.disabled = true)
+    if (playerScore > computerScore) {
+        winnerPhrase.textContent = "YOU WON !!! 🙂‍↔, Press Reset Button to Play Again."
     }
-    else if (humanChoice == "rock" && computerChoice == "paper") {
-        cs++;
+    else if (playerScore == computerScore) {
+        winnerPhrase.textContent = "T I E 🟰, Press Reset Button to Play Again."
+    }
+    else {
+        winnerPhrase.textContent = "You Lost !!! 😒, Press Reset Button to Play Again."
+    }
+}
+
+function playRound(playerChoice, computerChoice) {
+    rounds--;
+    computerInputField.style.cssText = "display: block;";
+    if (playerChoice === computerChoice) {
+        console.log("TIED... 🟰");
+        console.log("--------------------------------------");
+        computerInputField.textContent = computerChoice.toUpperCase();
+    }
+    else if (playerChoice == "rock" && computerChoice == "paper") {
         console.log("You Lost... 😒, Paper beats Rock");
+        console.log("--------------------------------------");
+        computerScore++;
+        computerInputField.textContent = computerChoice.toUpperCase();
     }
-    else if (humanChoice == "rock" && computerChoice == "scissors") {
-        hs++;
+    else if (playerChoice == "rock" && computerChoice == "scissors") {
         console.log("You Won... 😊, Rock beats Scissors");
+        console.log("--------------------------------------");
+        playerScore++;
+        computerInputField.textContent = computerChoice.toUpperCase();
     }
-    else if (humanChoice == "paper" && computerChoice == "rock") {
-        hs++;
+    else if (playerChoice == "paper" && computerChoice == "rock") {
         console.log("You Won... 😊, Paper beats Rock");
+        console.log("--------------------------------------");
+        playerScore++;
+        computerInputField.textContent = computerChoice.toUpperCase();
     }
-    else if (humanChoice == "paper" && computerChoice == "scissors") {
-        cs++;
+    else if (playerChoice == "paper" && computerChoice == "scissors") {
         console.log("You Lost... 😒, Scissors beats Paper");
+        console.log("--------------------------------------");
+        computerScore++;
+        computerInputField.textContent = computerChoice.toUpperCase();
     }
-    else if (humanChoice == "scissors" && computerChoice == "paper") {
-        hs++;
+    else if (playerChoice == "scissors" && computerChoice == "paper") {
         console.log("You Won... 😊, Scissors beats Paper");
+        console.log("--------------------------------------");
+        playerScore++;
+        computerInputField.textContent = computerChoice.toUpperCase();
     }
-    else if (humanChoice == "scissors" && computerChoice == "rock") {
-        cs++;
+    else if (playerChoice == "scissors" && computerChoice == "rock") {
         console.log("You Lost... 😒, Rock beats Scissors");
+        console.log("--------------------------------------");
+        computerScore++;
+        computerInputField.textContent = computerChoice.toUpperCase();
     }
 
-    console.log(`Human Score = ${hs}, Computer Score = ${cs}`);
+    updateScoreBoard();
 
-    if (hs > cs) {
-        console.log("YOU WON !!!... 😉");
-    }
-    else if (cs > hs) {
-        console.log("YOU LOST !!!... 🥺");
-    }
-    else {
-        console.log("TIE !!!");
+    if (rounds === 0) {
+        displayResult();
     }
 }
 
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
-    let n = parseInt(prompt("Enter the Number of Rounds: "));
-    if (n < 1 || isNaN(n)) {
-        console.log("Invalid Input ❌");
-        return;
-    }
-    for (let i = 0; i < n; i++) {
-        let computerChoice = getComputerChoice();
-        let humanChoice = getHumanChoice();
-        console.log(`Computer Random Selection in Round-${i + 1}: ${computerChoice}`);
-        console.log(`Your Selection in Round-${i + 1}: ${humanChoice}`);
-        if (computerChoice === humanChoice) {
-            console.log("TIED... 🟰");
-            console.log("--------------------------------------");
-        }
-        else if (humanChoice == "rock" && computerChoice == "paper") {
-            computerScore++;
-            console.log("You Lost... 😒, Paper beats Rock");
-            console.log("--------------------------------------");
-        }
-        else if (humanChoice == "rock" && computerChoice == "scissors") {
-            humanScore++;
-            console.log("You Won... 😊, Rock beats Scissors");
-            console.log("--------------------------------------");
-        }
-        else if (humanChoice == "paper" && computerChoice == "rock") {
-            humanScore++;
-            console.log("You Won... 😊, Paper beats Rock");
-            console.log("--------------------------------------");
-        }
-        else if (humanChoice == "paper" && computerChoice == "scissors") {
-            computerScore++;
-            console.log("You Lost... 😒, Scissors beats Paper");
-            console.log("--------------------------------------");
-        }
-        else if (humanChoice == "scissors" && computerChoice == "paper") {
-            humanScore++;
-            console.log("You Won... 😊, Scissors beats Paper");
-            console.log("--------------------------------------");
-        }
-        else if (humanChoice == "scissors" && computerChoice == "rock") {
-            computerScore++;
-            console.log("You Lost... 😒, Rock beats Scissors");
-            console.log("--------------------------------------");
-        }
-    }
 
-    console.log(`Human Score = ${humanScore}, Computer Score = ${computerScore}`);
-    if (humanScore > computerScore) {
-        console.log("Final State: YOU WON !!!... 😉");
-    }
-    else if (computerScore > humanScore) {
-        console.log("Final State: YOU LOST !!!... 🥺");
-    }
-    else {
-        console.log("Final State: TIE !!!... 🟰");
-    }
+// The Main Program 
+function main() {
+    roundsButton.addEventListener("click", function () {
+        rounds = roundsInput.value;
+        // Empty Input Field
+        if (rounds === 0) {
+            alert("ERROR !! ❌, Empty Input Field...");
+            location.reload(); //Reloads the Current Page
+        }
+        else if (rounds < 1) {
+            alert("Invalid Input ❌ Please Enter a Valid Number (Greater than 0)");
+            location.reload();
+        }
+        else {
+            roundsInput.disabled = true;
+            roundsButton.disabled = true;
+            roundsInput.style.cssText = "font-weight: bold; text-align: center;";
+            alert(`Game Started With Rounds -> ${rounds}`);
+            scoresBox.style.cssText = "display: flex;";
+        }
+    });
+
+    //Get the User Input and Calculate the Scores.
+    inputButtons.forEach(button => {
+        button.addEventListener("click", function (e) {
+            if (rounds === 0) {
+                alert("ERROR !!! ❌, Fill  'number of rounds'  box First...");
+                location.reload(); //Reloads the Current Page
+            }
+            else {
+                console.log("Button Clicked: ", e.target.id);
+                console.log("Round of -- ", rounds);
+                console.log("Human Score Type = ", typeof playerScore, "Value = ", playerScore);
+                console.log("Computer Score = ", typeof computerScore, "Value = ", computerScore);
+                computerInputField.style.cssText = "display: block;";
+                playRound(e.target.id, getComputerChoice());
+            }
+        });
+    })
+
+    //Resets
+    resetButton.addEventListener("click", function () {
+        location.reload(); //Reloads the Current Page
+    });
 }
 
-playGame();
+main();
