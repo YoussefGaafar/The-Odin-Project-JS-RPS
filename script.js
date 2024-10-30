4afc5fd306de4094b1dec398585d6e70ffa32ea0
Youssef Gaafar <yousfjaafer@gmail.com>
2024-10-30 17:43:48 +0300

    ➕ ADD: Round Status Update

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const winnerBox = document.querySelector(".winner");
 const winnerPhrase = document.querySelector(".winner h2");
 const resetButton = document.querySelector(".winner button");
 const currentRound = document.querySelector(".rounds .current-round");
+const roundsStatus = document.querySelector(".inputs .rounds-status");
 
 let playerScore = parseInt(playerScoreSpan.textContent);
 let computerScore = parseInt(computerScoreSpan.textContent);
@@ -25,6 +26,40 @@ function updateScoreBoard() {
     computerScoreSpan.textContent = computerScore;
 }
 
+function updateRoundsStatus(playerChoice, computerChoice, currentRoundStatus) {
+    const newRoundStatus = document.createElement("h4");
+    newRoundStatus.textContent = `Round-${currentRoundNumber}:`;
+    const playerChoiceSpan = document.createElement('span');
+    playerChoiceSpan.classList.add('player-choice');
+    playerChoiceSpan.textContent = playerChoice.toUpperCase();
+
+    // Create the computer choice span
+    const computerChoiceSpan = document.createElement('span');
+    computerChoiceSpan.classList.add('computer-choice');
+    computerChoiceSpan.textContent = computerChoice.toUpperCase();
+
+    //Checks the winner of the current round
+    if (currentRoundStatus === 0) { //Tie
+        newRoundStatus.appendChild(document.createTextNode(" TIE, "));
+        newRoundStatus.appendChild(playerChoiceSpan);
+        newRoundStatus.appendChild(document.createTextNode(" 🟰 "));
+        newRoundStatus.appendChild(computerChoiceSpan);
+    }
+    else if (currentRoundStatus === 1) { // Player Won
+        newRoundStatus.appendChild(document.createTextNode(" WIN✅,  "));
+        newRoundStatus.appendChild(playerChoiceSpan);
+        newRoundStatus.appendChild(document.createTextNode(" beats "));
+        newRoundStatus.appendChild(computerChoiceSpan);
+    }
+    else { // Computer Won
+        newRoundStatus.appendChild(document.createTextNode(" Lost❌,  "));
+        newRoundStatus.appendChild(computerChoiceSpan);
+        newRoundStatus.appendChild(document.createTextNode(" beats "));
+        newRoundStatus.appendChild(playerChoiceSpan);
+    }
+    roundsStatus.appendChild(newRoundStatus);
+}
+
 function replaceTextAndAnimate(newText) {
     computerInputField.textContent = newText; // Replace text
     computerInputField.style.animation = 'none'; // Reset animation
@@ -37,13 +72,13 @@ function displayResult() {
     winnerBox.style.cssText = "display: block;";
     inputButtons.forEach(button => button.disabled = true)
     if (playerScore > computerScore) {
-        winnerPhrase.textContent = "YOU WON !!! 🙂‍↔, Press Reset Button to Play Again."
+        winnerPhrase.textContent = "YOU WON !!! 🙂‍↔, Press reset button to play again."
     }
     else if (playerScore == computerScore) {
-        winnerPhrase.textContent = "T I E 🟰, Press Reset Button to Play Again."
+        winnerPhrase.textContent = "T I E 🟰, Press reset button to play again."
     }
     else {
-        winnerPhrase.textContent = "You Lost !!! 😒, Press Reset Button to Play Again."
+        winnerPhrase.textContent = "You Lost !!! 😒, Press reset button to play again."
     }
 }
 
@@ -54,42 +89,56 @@ function playRound(playerChoice, computerChoice) {
         console.log("TIED... 🟰");
         console.log("--------------------------------------");
         replaceTextAndAnimate(computerChoice.toUpperCase());
+        const message = `Round: ${currentRoundNumber} -> TIED... 🟰, (${playerChoice.toUpperCase()} 🟰 ${computerChoice.toUpperCase()})`;
+        updateRoundsStatus(playerChoice, computerChoice, 0);
     }
     else if (playerChoice == "rock" && computerChoice == "paper") {
         console.log("You Lost... 😒, Paper beats Rock");
         console.log("--------------------------------------");
         computerScore++;
         replaceTextAndAnimate(computerChoice.toUpperCase());
+        const message = `Round: ${currentRoundNumber} -> LOST... ❌, (${playerChoice.toUpperCase()} vs ${computerChoice.toUpperCase()})`;
+        updateRoundsStatus(playerChoice, computerChoice), -1;
     }
     else if (playerChoice == "rock" && computerChoice == "scissors") {
         console.log("You Won... 😊, Rock beats Scissors");
         console.log("--------------------------------------");
         playerScore++;
         replaceTextAndAnimate(computerChoice.toUpperCase());
+        const message = `Round: ${currentRoundNumber} -> WIN... ✅, (${playerChoice.toUpperCase()} beats ${computerChoice.toUpperCase()})`;
+        updateRoundsStatus(playerChoice, computerChoice, 1);
     }
     else if (playerChoice == "paper" && computerChoice == "rock") {
         console.log("You Won... 😊, Paper beats Rock");
         console.log("--------------------------------------");
         playerScore++;
         replaceTextAndAnimate(computerChoice.toUpperCase());
+        const message = `Round: ${currentRoundNumber} -> WIN... ✅, (${playerChoice.toUpperCase()} beats ${computerChoice.toUpperCase()})`;
+        updateRoundsStatus(playerChoice, computerChoice), 1;
     }
     else if (playerChoice == "paper" && computerChoice == "scissors") {
         console.log("You Lost... 😒, Scissors beats Paper");
         console.log("--------------------------------------");
         computerScore++;
         replaceTextAndAnimate(computerChoice.toUpperCase());
+        const message = `Round: ${currentRoundNumber} -> LOST... ❌, (${playerChoice.toUpperCase()} vs ${computerChoice.toUpperCase()})`;
+        updateRoundsStatus(playerChoice, computerChoice, -1);
     }
     else if (playerChoice == "scissors" && computerChoice == "paper") {
         console.log("You Won... 😊, Scissors beats Paper");
         console.log("--------------------------------------");
         playerScore++;
         replaceTextAndAnimate(computerChoice.toUpperCase());
+        const message = `Round: ${currentRoundNumber} -> WIN... ✅, (${playerChoice.toUpperCase()} beats ${computerChoice.toUpperCase()})`;
+        updateRoundsStatus(playerChoice, computerChoice, 1);
     }
     else if (playerChoice == "scissors" && computerChoice == "rock") {
         console.log("You Lost... 😒, Rock beats Scissors");
         console.log("--------------------------------------");
         computerScore++;
         replaceTextAndAnimate(computerChoice.toUpperCase());
+        const message = `Round: ${currentRoundNumber} -> LOST... ❌, (${playerChoice.toUpperCase()} vs ${computerChoice.toUpperCase()})`;
+        updateRoundsStatus(playerChoice, computerChoice, -1);
     }
 
     updateScoreBoard();
@@ -121,6 +170,7 @@ function main() {
             scoresBox.style.cssText = "display: flex;";
             currentRound.textContent = `Current Round: ${currentRoundNumber}`;
             currentRound.style.cssText = "display: block;";
+            roundsStatus.style.cssText = "display: flex;";
         }
     });
 
